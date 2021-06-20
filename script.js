a08368ed5ec680722615d55f199e2e24755858b5
attila <attilavigh.contact@gmail.com>
2021-06-20 23:08:54 +0100

    Playing with the DOM

---
+++ script.js
@@ -0,0 +1,10 @@
+let userInput = document.querySelector("#userInput")
+let display = document.querySelector(".display") 
+
+// console.debug("---< debug >---", userInput );
+// userInput.addEventListener( "change", (event) => {
+//     display.innerHTML = event.target.value 
+//     console.log( "---< userInput >---" );
+//     console.log( "---< event >---" );
+//     console.log( event );
+// })
